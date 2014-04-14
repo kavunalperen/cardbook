60292a5e0f2005b5f1f085251edeef41b5beec93
Alperen Kavun <alperen@halici.com.tr>
2014-04-14 20:47:14 +0300

    info view implementation is finished

--- iOS/Cardbook/Cardbook/ViewControllers/CBInfoViewController.m
+++ iOS/Cardbook/Cardbook/ViewControllers/CBInfoViewController.m
@@ -15,18 +15,49 @@
 @end
 
 @implementation CBInfoViewController
-
+{
+    UILabel* companyNameLabel;
+    UILabel* companyAboutTextLabel;
+}
+- (CGRect) backgroundFrame
+{
+    CGFloat backHeight;
+    if (IS_IPHONE_5) {
+        backHeight = 436.0;
+    } else {
+        backHeight = 348.0;
+    }
+    return CGRectMake(15.0, 69.0, 290.0, backHeight);
+}
 - (CGRect) scrollViewFrame
 {
-    return CGRectMake(15.0, 69.0, 290.0, SCREEN_SIZE.height-69.0);
+    CGFloat backHeight;
+    if (IS_IPHONE_5) {
+        backHeight = 436.0;
+    } else {
+        backHeight = 348.0;
+    }
+    return CGRectMake(15.0, 0.0, 260.0, backHeight);
+}
+- (CGRect) companyNameLabelFrame
+{
+    return CGRectMake(0.0, 0.0, 260.0, 35.0);
+}
+- (CGRect) companyAboutLabelFrame
+{
+    NSString* aboutStr = self.currentCardInfo.aboutText;
+    
+    CGSize labelSize = [[CBUtil sharedInstance] text:aboutStr sizeWithFont:CARD_INFO_ABOUT_TEXT_FONT constrainedToSize:CGSizeMake(260.0, 10000.0)];
+    
+    labelSize = CGSizeMake(ceilf(labelSize.width), ceilf(labelSize.height));
+    
+    return CGRectMake(0.0, 40.0, 260.0, labelSize.height);
 }
 - (CGSize) scrollViewContentSize
 {
-    return CGSizeMake(290.0, 435.0+49.0+15.0);
-}
-- (CGRect) scrollViewBackFrame
-{
-    return CGRectMake(0.0, 0.0, 290.0, 435.0);
+    CGRect labelFrame = [self companyAboutLabelFrame];
+    
+    return CGSizeMake(260.0, labelFrame.origin.y+labelFrame.size.height+15.0);
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -77,19 +108,42 @@
 }
 - (void) initCommonViews
 {
+    self.view.userInteractionEnabled = YES;
+    UIImageView* backView = [[UIImageView alloc] initWithFrame:[self backgroundFrame]];
+    [backView setBackgroundColor:[UIColor clearColor]];
+    [backView setImage:[UIImage imageNamed:@"info_back.png"]];
+    backView.userInteractionEnabled = YES;
+    [self.view addSubview:backView];
+    
     self.scrollView = [[UIScrollView alloc] initWithFrame:[self scrollViewFrame]];
     [self.scrollView setBackgroundColor:[UIColor clearColor]];
-    [self.view addSubview:self.scrollView];
     [self.scrollView setContentSize:[self scrollViewContentSize]];
-    [self.scrollView setShowsVerticalScrollIndicator:NO];
+    [self.scrollView setShowsVerticalScrollIndicator:YES];
+    self.scrollView.userInteractionEnabled = YES;
+    [backView addSubview:self.scrollView];
     
-    UIImageView* backView = [[UIImageView alloc] initWithFrame:[self scrollViewBackFrame]];
-    [backView setBackgroundColor:[UIColor clearColor]];
-    [backView setImage:[UIImage imageNamed:@"main_bg.png"]];
-    [self.scrollView addSubview:backView];
+    companyNameLabel = [[UILabel alloc] initWithFrame:[self companyNameLabelFrame]];
+    [companyNameLabel setBackgroundColor:[UIColor clearColor]];
+    [companyNameLabel setFont:MY_CAMPAIGNS_DETAILS_TITLE_LABEL_FONT];
+    [companyNameLabel setTextColor:MY_CAMPAIGNS_DETAILS_TITLE_LABEL_TEXT_COLOR];
+    [self.scrollView addSubview:companyNameLabel];
+    
+    companyAboutTextLabel = [[UILabel alloc] initWithFrame:[self companyAboutLabelFrame]];
+    companyAboutTextLabel.backgroundColor = [UIColor clearColor];
+    companyAboutTextLabel.numberOfLines = 0;
+    companyAboutTextLabel.font = CARD_INFO_ABOUT_TEXT_FONT;
+    companyAboutTextLabel.textColor = CARD_INFO_ABOUT_TEXT_COLOR;
+    [self.scrollView addSubview:companyAboutTextLabel];
 }
 - (void) configureViews
 {
+    NSLog(@"configure Views");
+    
+    companyAboutTextLabel.frame = [self companyAboutLabelFrame];
+    self.scrollView.contentSize = [self scrollViewContentSize];
+    
+    companyNameLabel.text = self.currentCardDetail.companyName;
+    companyAboutTextLabel.text = self.currentCardInfo.aboutText;
     
 }
 - (void) goBackToCardDetail
