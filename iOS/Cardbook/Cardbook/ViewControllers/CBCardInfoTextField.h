6d4a715e4a648fa933885ce005801759c4f97b47
Alperen Kavun <alperen@halici.com.tr>
2014-04-16 22:20:44 +0300

    added new file

---
+++ iOS/Cardbook/Cardbook/ViewControllers/CBCardInfoTextField.h
@@ -0,0 +1,13 @@
+//
+//  CBCardInfoTextField.h
+//  Cardbook
+//
+//  Created by Alperen Kavun on 14.04.2014.
+//  Copyright (c) 2014 kavun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBCardInfoTextField : UITextField
+
+@end
